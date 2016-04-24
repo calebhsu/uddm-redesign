87796e45959d07b7968e418ee2673b98ec6442d1
Caleb Hsu <chialo.hsu@colorado.edu>
2016-04-23 21:23:06 -0600

    Applied accessible footer to all pages

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   $('.slider').slider({full_width: true});
+  $('.parallax').parallax();
   $('.button-collapse').sideNav();
   $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
 });
-$('.button-collapse').sideNav();
+
