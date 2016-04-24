9493c8e2a1317a13e7c7c02f27ee5d2a06bbc0c3
Caleb Hsu <chialo.hsu@colorado.edu>
2016-04-23 21:19:36 -0600

    Applied responsive navbar to all pages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
   $('.slider').slider({full_width: true});
+  $('.button-collapse').sideNav();
+  $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
 });
+$('.button-collapse').sideNav();
